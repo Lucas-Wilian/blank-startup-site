9a0d540fea49cc2b5140716e29f775db47d4d456
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-02 20:57:10 -0300

    rename file

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,25 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      bg: string;
+      bgSecundary: string;
+      bgHeader: string;
+      outline: string;
+      orage: string;
+      cyan: string;
+      yellow: string;
+      green: string;
+      white: string;
+      pinky: string;
+      bgOrage: string;
+      bgRed: string;
+      bgGreen: string;
+      bgBlue: string;
+      bgSideBar: string;
+      bgHeaderButton: string;
+      bgHeaderButtonHover: string;
+    };
+  }
+}
