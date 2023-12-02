b65e2272c5b64a0ceebf6356011cbbd1410d3067
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-02 20:57:56 -0300

    styled layout

--- src/components/Layout/styled.ts
+++ src/components/Layout/styled.ts
@@ -1,3 +1,6 @@
 import { styled } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 0rem 6rem;
+  background: ${({ theme }) => theme.colors.bg};
+`;
