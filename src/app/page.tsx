af3ae5bd1aaa6b01d76198e3514a5706626a73d8
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-02 20:56:54 -0300

    feat: app provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 import Layout from '@/components/Layout/Layout';
+import { AppProvider } from '@/context/theme';
 
 export default function Home() {
   return (
-    <div>
+    <AppProvider>
       <Layout>
         <p>Blank</p>
       </Layout>
-    </div>
+    </AppProvider>
   );
 }
