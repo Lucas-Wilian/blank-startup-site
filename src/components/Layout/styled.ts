99544fb03386264811301ac981de06232dd757d1
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-03 17:12:08 -0300

    fix: 100vh styled

--- src/components/Layout/styled.ts
+++ src/components/Layout/styled.ts
@@ -1,6 +1,8 @@
 import { styled } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+`;
 
 export const Section = styled.div`
   padding: 0rem 6rem;
