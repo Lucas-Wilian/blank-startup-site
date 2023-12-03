a1b98add0b3c23cd050938b6cde0ff4c007696f3
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-03 17:11:26 -0300

    feat: Dropdown hover menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,21 +7,49 @@ export default function Header() {
   return (
     <C.Container>
       <Logo />
-      <C.DropMenu>
-        <span>
-          Sobre <C.IconArrow />
-        </span>
-      </C.DropMenu>
-      <C.DropMenu>
-        <span>
-          Produtos <C.IconArrow />
-        </span>
-      </C.DropMenu>
-      <C.DropMenu>
-        <span>
-          Social <C.IconArrow />
-        </span>
-      </C.DropMenu>
+
+      <C.DropdownWrapper>
+        <C.DropMenu>
+          <span>
+            Sobre <C.IconArrow />
+          </span>
+        </C.DropMenu>
+        <C.DropdownContent>
+          {/* <C.DropdownLink>Link1</C.DropdownLink>
+          <C.DropdownLink>Link2</C.DropdownLink>
+          <C.DropdownLink>Link3</C.DropdownLink>
+          <C.DropdownLink>Link4</C.DropdownLink> */}
+        </C.DropdownContent>
+      </C.DropdownWrapper>
+
+      <C.DropdownWrapper>
+        <C.DropMenu>
+          <span>
+            Produtos <C.IconArrow />
+          </span>
+        </C.DropMenu>
+        <C.DropdownContent>
+          {/* <C.DropdownLink>Link1</C.DropdownLink>
+          <C.DropdownLink>Link2</C.DropdownLink>
+          <C.DropdownLink>Link3</C.DropdownLink>
+          <C.DropdownLink>Link4</C.DropdownLink> */}
+        </C.DropdownContent>
+      </C.DropdownWrapper>
+
+      <C.DropdownWrapper>
+        <C.DropMenu>
+          <span>
+            Social <C.IconArrow />
+          </span>
+        </C.DropMenu>
+        <C.DropdownContent>
+          {/* <C.DropdownLink>Link1</C.DropdownLink>
+          <C.DropdownLink>Link2</C.DropdownLink>
+          <C.DropdownLink>Link3</C.DropdownLink>
+          <C.DropdownLink>Link4</C.DropdownLink> */}
+        </C.DropdownContent>
+      </C.DropdownWrapper>
+
       <C.ButtonContainer>
         <Button>
           <p>Saiba +</p>
