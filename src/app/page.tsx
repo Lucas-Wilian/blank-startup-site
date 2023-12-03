bb7373244b20985dce3e875edc8e2e423e1cd3f2
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-03 14:42:08 -0300

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 'use client';
-import Layout from '@/components/Layout/Layout';
 import { AppProvider } from '@/context/theme';
 
 export default function Home() {
   return (
     <AppProvider>
-      <Layout>
-        <p>Blank</p>
-      </Layout>
+      <p>🔥🚀👻</p>
     </AppProvider>
   );
 }
