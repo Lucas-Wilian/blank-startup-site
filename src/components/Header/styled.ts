4d5227e56f2a9f7079ba255cd898585ebeca7a56
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-03 17:11:49 -0300

    styled dropdown menu header

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -35,8 +35,51 @@ export const DropMenu = styled.div`
   }
 `;
 
+export const DropdownWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+export const DropdownContent = styled.div`
+  display: none;
+  background: ${({ theme }) => theme.colors.blk_theme_elevation_3};
+  min-width: 300px;
+  min-height: 300px;
+  position: absolute;
+  top: 100%;
+  border-radius: 5px;
+  border: solid 0.05rem
+    ${({ theme }) => theme.colors.blk_theme_toggle_opacity};
+  z-index: 1;
+  box-shadow: 0 0 4px 4px
+    ${({ theme }) => theme.colors.blk_theme_blue_light};
+  ${DropdownWrapper}:hover & {
+    display: block;
+    transition: all 0.3s ease;
+  }
+  left: 0;
+  right: auto;
+`;
+
+export const DropdownLink = styled.span`
+  display: block;
+  color: #000;
+  text-decoration: none;
+  padding: 17px;
+  font-size: 18px;
+
+  &:hover {
+    background: #ddd;
+  }
+`;
+
 export const IconArrow = styled(IconBxChevronDown)`
   width: 23px;
   height: 23px;
   flex-shrink: 0;
+  transition: transform 0.4s ease-in-out;
+
+  ${DropdownWrapper}:hover & {
+    transform: rotate(180deg);
+  }
 `;
