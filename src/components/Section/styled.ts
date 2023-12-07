23d1e2ec8b3568af0828bc4e912178cd2377a3f8
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-06 22:49:14 -0300

    fix: temporary styling

--- src/components/Section/styled.ts
+++ src/components/Section/styled.ts
@@ -1,4 +1,12 @@
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
+
+const ButtonStyle = css`
+  font-size: 1.7rem;
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.colors.blk_theme_btn_bg};
+  font-weight: bold;
+`;
 
 export const Container = styled.div`
   height: 100vh;
@@ -15,6 +23,25 @@ export const Text = styled.h2`
   color: ${({ theme }) => theme.colors.blk_theme_contrast};
 `;
 
-export const SectionButtonGlogals = styled.div``;
+export const SectionButton = styled.div`
+  display: flex;
+  align-items: center;
+  height: 90px;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
 
 export const ButtonGlogalSection = styled.div``;
+
+export const Button = styled.button`
+  ${ButtonStyle}
+  background: ${({ theme }) => theme.colors.blk_theme_btn_bg};
+`;
+
+export const ButtonUnderline = styled.button`
+  ${ButtonStyle}
+  background:transparent;
+  color: ${({ theme }) => theme.colors.blk_theme_btn_bg};
+`;
