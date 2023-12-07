dfa461ad7517ceb9435a231a365339c02268c570
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-06 22:49:51 -0300

    removed ButtonGlobal

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import * as C from './styled';
-import ButtonGlogal from '../Button/Button';
 
 export default function Section() {
   return (
     <C.Container>
       <C.Text>Softwares modernos para sua empresa</C.Text>
-      <C.SectionButtonGlogals>
-        <C.ButtonGlogalSection>
-          {/* <ButtonGlogal>
+      <C.SectionButton>
+        {/* <ButtonGlogal>
             <p>Começar </p>
           </ButtonGlogal> */}
-          <ButtonGlogal>Cadastrar para noticias</ButtonGlogal>
-        </C.ButtonGlogalSection>
-      </C.SectionButtonGlogals>
+        <C.ButtonUnderline>Cadastrar para noticias</C.ButtonUnderline>
+        <C.Button>Começar </C.Button>
+      </C.SectionButton>
     </C.Container>
   );
 }
