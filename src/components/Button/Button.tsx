48953bb3c16f843e17ed912f7e30042a9980f34f
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-07 23:16:11 -0300

    feat: Props inferface Global Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import * as C from './styled';
 
+interface PropsGlobal {
+  text: string;
+  font_size?: string;
+  background?: boolean;
+  width?: string;
+  height?: string;
+}
+
 export default function ButtonGlogal({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <C.Container>{children}</C.Container>;
+  text,
+  font_size,
+  background,
+  width,
+  height,
+}: PropsGlobal) {
+  return (
+    <C.Container
+      background={background}
+      font_size={font_size}
+      width={width}
+      height={height}
+    >
+      <p>{text}</p>
+    </C.Container>
+  );
 }
