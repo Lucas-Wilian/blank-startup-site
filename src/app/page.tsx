f108d7c2a5d1861d164bb2c67fa5e5bf896c88f9
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-10 18:46:46 -0300

    feat: new components home page HeaderFixed/SectionYoutube

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 import Carrossel from '@/components/Carrossel/Carrossel';
+import HeaderFixed from '@/components/HeaderFixed/HeaderFixed';
 import Section from '@/components/Section/Section';
+import SectionYoutube from '@/components/SectionYoutube/SectionYoutube';
 import { AppProvider } from '@/context/theme';
 
 export default function Home() {
   return (
     <AppProvider>
+      <HeaderFixed />
       <Section />
       <Carrossel />
+      <SectionYoutube />
     </AppProvider>
   );
 }
