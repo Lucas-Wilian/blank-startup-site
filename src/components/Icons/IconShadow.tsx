f79a42b1a69e690f2e2e932164b2bd4af0245f4a
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-11 21:28:19 -0300

    feat: new icon

---
+++ src/components/Icons/IconShadow.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+function IconShadow(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width='1em'
+      height='1em'
+      viewBox='0 0 122.436 39.744'
+      enable-background='new 0 0 122.436 39.744'
+      fill='currentColor'
+      {...props}
+    >
+      <ellipse cx='61.128' cy='19.872' rx='49.25' ry='8.916' />
+    </svg>
+  );
+}
+
+export default IconShadow;
