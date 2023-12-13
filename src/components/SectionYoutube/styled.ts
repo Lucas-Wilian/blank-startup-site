893d9bc8c24b8602457ffba78f9e59b8d18599c5
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-12 23:13:16 -0300

    fix: background const Container Style

--- src/components/SectionYoutube/styled.ts
+++ src/components/SectionYoutube/styled.ts
@@ -2,6 +2,7 @@ import { css, styled } from 'styled-components';
 
 const ContainerStyle = css`
   border-radius: 8px;
+  background: ${({ theme }) => theme.colors.blk_theme_primary_light};
   border: 0.1rem solid
     ${({ theme }) => theme.colors.blk_theme_toggle_btn_background};
 `;
@@ -28,7 +29,6 @@ export const VideoPrimary = styled.div`
   ${ContainerStyle}
   width: 100%;
   height: 510px;
-  background: ${({ theme }) => theme.colors.blk_theme_primary_light};
 `;
 
 export const ContainerSocial = styled.div`
@@ -42,7 +42,6 @@ export const ContainerSocial = styled.div`
 
 export const SocialSection = styled.div`
   ${ContainerStyle}
-  background: ${({ theme }) => theme.colors.blk_theme_primary_light};
   height: 100%;
   flex: 1;
 `;
@@ -60,6 +59,4 @@ export const VideoSecondary = styled.div`
   ${ContainerStyle}
   width: 100%;
   height: 50%;
-
-  background: ${({ theme }) => theme.colors.blk_theme_primary_light};
 `;
