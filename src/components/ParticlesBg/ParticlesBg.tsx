d3304cc2ab9a0d8ce4d83a0db4447091c54c5d66
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-14 23:49:54 -0300

    feat: new const /lib

--- src/components/ParticlesBg/ParticlesBg.tsx
+++ src/components/ParticlesBg/ParticlesBg.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Particles from 'react-tsparticles';
 import particlesConfig from '../config/particles-config';
+import { loadSlim } from 'tsparticles-slim';
 
 export default function ParticlesBg() {
-  return <Particles params={particlesConfig} />;
+  const particlesInit = useCallback(async (engine: any) => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container: any) => {
+    console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      id='tsparticles'
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={particlesConfig}
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        gap: 1,
+      }}
+    />
+  );
 }
