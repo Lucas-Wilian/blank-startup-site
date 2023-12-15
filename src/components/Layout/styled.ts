dd9056d655687b7b463cb786e141bc20c6c24adb
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-14 23:49:19 -0300

    feat: ContainerHeader styled z-index

--- src/components/Layout/styled.ts
+++ src/components/Layout/styled.ts
@@ -7,3 +7,8 @@ export const Container = styled.div`
 export const Section = styled.div`
   background: ${({ theme }) => theme.colors.blk_theme_background};
 `;
+
+export const ContainerHeader = styled.div`
+  z-index: 4;
+  position: relative;
+`;
