9149cbd9004f22bc89a8fed29754b247f4c9bf38
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-14 23:50:36 -0300

    import ParticlesBg

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import * as C from './styled';
 import ButtonGlogal from '../Button/Button';
+import ParticlesBg from '../ParticlesBg/ParticlesBg';
 
 export default function Section() {
   return (
     <C.Container>
+      <ParticlesBg />
       <C.Text>Softwares modernos para sua empresa</C.Text>
       <C.SectionButton>
         <ButtonGlogal
