4204ad435b7869757385c1dadf92d1e62fa2500d
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-16 18:08:29 -0300

    icon, social container, container social

--- src/components/SectionYoutube/styled.ts
+++ src/components/SectionYoutube/styled.ts
@@ -1,8 +1,12 @@
 import { css, styled } from 'styled-components';
+import IconYoutube from '../Icons/IconYoutube';
+import IconLinkedin from '../Icons/IconLinkedin';
+import IconInstagram from '../Icons/IconInstagrams';
 
 const ContainerStyle = css`
   border-radius: 8px;
-  background: ${({ theme }) => theme.colors.blk_theme_primary_light};
+  /* background: ${({ theme }) =>
+    theme.colors.blk_theme_primary_light}; */
   cursor: pointer;
   border: 0.1rem solid
     ${({ theme }) => theme.colors.blk_theme_toggle_btn_background};
@@ -11,6 +15,20 @@ const ContainerStyle = css`
     transform: scale(0.98);
     box-shadow: 0 0 4px 4px
       ${({ theme }) => theme.colors.blk_theme_blue_light};
+  }
+`;
+
+const SocialContainer = css`
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  p {
+    color: #fff;
   }
 `;
 
@@ -53,6 +71,21 @@ export const SocialSection = styled.div`
   flex: 1;
 `;
 
+export const ContainerYoutube = styled.div`
+  ${SocialContainer}
+  background: red;
+`;
+
+export const ContainerLinkedIn = styled.div`
+  ${SocialContainer}
+  background: #5865f2;
+`;
+
+export const ContainerInstagram = styled.div`
+  ${SocialContainer}
+  background: #9147ff;
+`;
+
 export const ContainerSecondary = styled.div`
   display: flex;
   flex-direction: column;
@@ -66,4 +99,24 @@ export const VideoSecondary = styled.div`
   ${ContainerStyle}
   width: 100%;
   height: 50%;
+`;
+
+const IconStyle = css`
+  width: 4rem;
+  height: 4rem;
+  flex-shrink: 0;
+  fill: #fff;
+`;
+
+export const YotubeIcon = styled(IconYoutube)`
+  ${IconStyle}
+`;
+
+export const LinkedInIcon = styled(IconLinkedin)`
+  ${IconStyle}
+`;
+
+export const InstagramIcon = styled(IconInstagram)`
+  ${IconStyle}
+  color: #fff;
 `;
