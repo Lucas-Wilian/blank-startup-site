b41a6336a8489d22a049f9dce428efa770357d46
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-17 14:23:15 -0300

    feat: footer style/component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,5 +2,57 @@ import React from 'react';
 import * as C from './styled';
 
 export default function Footer() {
-  return <C.Container>Footer</C.Container>;
+  return (
+    <C.Container>
+      <C.SectionFooter>
+        <C.SectionFooterLinks>
+          <C.TitleFooter>Tutorial</C.TitleFooter>
+          <C.TextFooterLinks>Blog</C.TextFooterLinks>
+          <C.TextFooterLinks>Youtube</C.TextFooterLinks>
+          <C.TextFooterLinks>LinkedIn</C.TextFooterLinks>
+          <C.TextFooterLinks>Instagram</C.TextFooterLinks>
+          <C.TextFooterLinks>GitHub</C.TextFooterLinks>
+        </C.SectionFooterLinks>
+        <C.SectionFooterLinks>
+          <C.TitleFooter>Sobre</C.TitleFooter>
+          <C.TextFooterLinks>Softwares</C.TextFooterLinks>
+          <C.TextFooterLinks>Instagram</C.TextFooterLinks>
+          <C.TextFooterLinks>GitHub</C.TextFooterLinks>
+          <C.TextFooterLinks>LinkedIn</C.TextFooterLinks>
+        </C.SectionFooterLinks>
+        <C.SectionFooterLinks>
+          <C.TitleFooter>Contato</C.TitleFooter>
+          <C.TextFooterLinks>
+            Whatsapp: (99) 9 8510-5141
+          </C.TextFooterLinks>
+          <C.TextFooterLinks>
+            Email: blank-startup@gmail.com
+          </C.TextFooterLinks>
+          <C.TextFooterLinks>Instagram</C.TextFooterLinks>
+        </C.SectionFooterLinks>
+        <C.SectionFooterLinks>
+          <C.TitleFooter>Termos</C.TitleFooter>
+          <C.TextFooterLinks>
+            Politica de Privacidade
+          </C.TextFooterLinks>
+          <C.TextFooterLinks>
+            Preferências de Cookies
+          </C.TextFooterLinks>
+        </C.SectionFooterLinks>
+        <C.SectionFooterLinks>
+          <C.TitleFooter>
+            Mantenha-se atualizado sobre novos lançamentos, promoções
+            e recursos, guias e estudos.
+          </C.TitleFooter>
+          <form>
+            <input placeholder='Telefone: ' />
+            <button>Cadastrar</button>
+          </form>
+        </C.SectionFooterLinks>
+      </C.SectionFooter>
+      <C.Footer>
+        <p>© 2023 BLANK Startup, Inc.</p>
+      </C.Footer>
+    </C.Container>
+  );
 }
--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -2,8 +2,99 @@ import { styled } from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 400px;
-  padding: 0rem 6rem;
+  height: auto;
   position: relative;
   background: ${({ theme }) => theme.colors.blk_theme_border};
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SectionFooter = styled.div`
+  padding: 3rem 6rem;
+  height: inherit;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  /* flex-wrap: wrap; */
+  justify-content: center;
+  width: 100%;
+`;
+
+export const SectionFooterLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  width: 250px;
+  form {
+    height: 32px;
+    position: relative;
+    display: flex;
+    width: 250px;
+    input {
+      border: 0;
+      max-width: 240px;
+
+      background: ${({ theme }) =>
+        theme.colors.blk_theme_toggle_opacity};
+      height: 100%;
+      border-radius: 6px;
+      color: ${({ theme }) => theme.colors.blk_theme_text_code};
+      font-size: 14px;
+      padding: 0 85px 0 12px;
+      outline: none;
+    }
+    button {
+      background: ${({ theme }) =>
+        theme.colors.blk_theme_background_hover};
+      font-size: 12px;
+      border-radius: 4px;
+      position: absolute;
+      padding: 2px 6px;
+      right: 6%;
+      top: 20%;
+      cursor: pointer;
+      color: ${({ theme }) => theme.colors.blk_theme_text_code};
+      border: 1px solid
+        ${({ theme }) => theme.colors.blk_theme_text_code};
+      &:hover {
+        background: ${({ theme }) =>
+          theme.colors.blk_theme_border_secondary};
+      }
+    }
+  }
+`;
+
+export const TextFooterLinks = styled.span`
+  font-size: 12px;
+  cursor: pointer;
+  font-weight: bold;
+  width: 100%;
+
+  text-align: justify;
+  color: ${({ theme }) => theme.colors.blk_theme_btn_bg};
+  &:hover {
+    color: ${({ theme }) => theme.colors.blk_theme_btn_hover_bg};
+  }
+`;
+
+export const TitleFooter = styled.div`
+  width: 100%;
+  font-size: 0.96rem;
+  color: ${({ theme }) => theme.colors.blk_theme_text_code};
+  margin-bottom: 10px;
+  font-weight: bold;
+`;
+
+export const Footer = styled.div`
+  height: 40px;
+  width: 100%;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  background: ${({ theme }) => theme.colors.blk_theme_primary_light};
+  p {
+    font-size: 12px;
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.blk_theme_text};
+  }
 `;
