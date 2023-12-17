73f346d970ab79c82315aaa078494bf23b423403
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-17 14:21:48 -0300

    new component home page: SectionPlatarform

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import HeaderFixed from '@/components/HeaderFixed/HeaderFixed';
 import LayoutBg from '@/components/LayoutBg/LayoutBg';
 import Section from '@/components/Section/Section';
 import SectionFooter from '@/components/SectionFooter/SectionFooter';
+import SectionPlataform from '@/components/SectionPlataform/SectionPlataform';
 import SectionProjects from '@/components/SectionProjects/SectionProjects';
 import SectionYoutube from '@/components/SectionYoutube/SectionYoutube';
 import { AppProvider } from '@/context/theme';
@@ -16,6 +17,7 @@ export default function Home() {
       <LayoutBg>
         <Carrossel />
         <SectionYoutube />
+        <SectionPlataform />
         <SectionProjects />
         <SectionFooter />
       </LayoutBg>
