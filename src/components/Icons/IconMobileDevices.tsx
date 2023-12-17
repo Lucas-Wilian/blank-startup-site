e004bcfd582e781081c5d237d97561edc701fa87
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-17 18:39:10 -0300

    feat: iconDiagram, IconMobile

---
+++ src/components/Icons/IconMobileDevices.tsx
@@ -0,0 +1,18 @@
+// icon:mobile-devices | Zondicons http://www.zondicons.com | Steve Schoger
+import * as React from 'react';
+
+function IconMobileDevices(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox='0 0 20 20'
+      fill='currentColor'
+      height='1em'
+      width='1em'
+      {...props}
+    >
+      <path d='M17 6V5h-2V2H3v14h5v4h3.25H11a2 2 0 01-2-2V8a2 2 0 012-2h6zm-5.75 14H3a2 2 0 01-2-2V2c0-1.1.9-2 2-2h12a2 2 0 012 2v4a2 2 0 012 2v10a2 2 0 01-2 2h-5.75zM11 8v8h6V8h-6zm3 11a1 1 0 100-2 1 1 0 000 2z' />
+    </svg>
+  );
+}
+
+export default IconMobileDevices;
