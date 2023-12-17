229723d9ef4649a90e0a92d7363405dd50be49d7
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-17 18:19:04 -0300

    new component header: DropRowContent, DropdownContentItems, DropdownItem, Icons, DrodCard

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,30 +15,129 @@ export default function Header() {
             {'Sobre' || <Skeleton />} <C.IconArrow />
           </span>
         </C.DropMenu>
-        <C.DropdownContent>
-          <p>Sobre</p>
+        <C.DropdownContent style={{ minWidth: '350px' }}>
+          <C.DrowRowContent>
+            <C.DropdownContentItems>
+              <C.DropdownItem>
+                <C.InfoIcon /> Sobre nós
+              </C.DropdownItem>
+              <C.DropdownItem>
+                <C.QuestIcon />
+                Por que BLANK?
+              </C.DropdownItem>
+              <C.DropdownItem>
+                <C.ConfIcon />
+                Como a funciona?
+              </C.DropdownItem>
+            </C.DropdownContentItems>
+            <C.Line />
+            <C.DropdownContentItems>
+              <C.DropdownItem>
+                <C.PuzzleIcon />
+                Casos de Uso
+              </C.DropdownItem>
+              <C.DropdownItem>
+                <C.SuportIcon />
+                Suporte
+              </C.DropdownItem>
+            </C.DropdownContentItems>
+          </C.DrowRowContent>
         </C.DropdownContent>
       </C.DropdownWrapper>
-
       <C.DropdownWrapper>
         <C.DropMenu>
           <span>
             {'Produtos' || <Skeleton />} <C.IconArrow />
           </span>
         </C.DropMenu>
-        <C.DropdownContent>
-          <p>Produtos</p>
+        <C.DropdownContent style={{ minWidth: '520px' }}>
+          <C.DrowRowContent>
+            <C.DropdownContentItems>
+              <C.DropTitle> Produtos</C.DropTitle>
+              <C.DropdownItem>
+                <C.BusinessIcon /> BLANK para Negócios
+              </C.DropdownItem>
+              <C.DropdownItem>
+                <C.UpdateIcon />
+                Atualizações constantes
+              </C.DropdownItem>
+
+              <C.DropdownItem>
+                <C.MoneyIcon />
+                Preços
+              </C.DropdownItem>
+              <C.DropdownItem>
+                <C.SuportIcon />
+                Suporte
+              </C.DropdownItem>
+            </C.DropdownContentItems>
+            <C.Line />
+            <C.DropdownContentItems>
+              <C.DropTitle>Soluções</C.DropTitle>
+              <C.DropdownItem>
+                <C.GlobeIcon />
+                Atendimento em Qualquer Lugar
+              </C.DropdownItem>
+              <C.DropdownItem>
+                <C.BagIcon />
+                Gestão Central
+              </C.DropdownItem>
+              <C.DropdownItem>
+                <C.SecurityIcon />
+                Protocolos de Segurança
+              </C.DropdownItem>
+            </C.DropdownContentItems>
+          </C.DrowRowContent>
+          <C.DrodCard>
+            <div>
+              <h4>Pronto para começar a usar os produtos BLANK?</h4>
+              <span>Damos suporte total para nossos clientes</span>
+            </div>
+            <section>
+              <ButtonGlogal
+                text='Começar'
+                font_size='0.9rem'
+                background={true}
+              />
+            </section>
+          </C.DrodCard>
         </C.DropdownContent>
       </C.DropdownWrapper>
 
       <C.DropdownWrapper>
         <C.DropMenu>
           <span>
-            {'Sobre' || <Skeleton />} <C.IconArrow />
+            {'Social' || <Skeleton />} <C.IconArrow />
           </span>
         </C.DropMenu>
-        <C.DropdownContent>
-          <p>Redes Sociais</p>
+        <C.DropdownContent style={{ minWidth: '350px' }}>
+          <C.DrowRowContent>
+            <C.DropdownContentItems>
+              <C.DropdownItem>
+                <C.UserIcon />
+                Comunidade BLANK
+              </C.DropdownItem>
+              <C.DropdownItem>
+                <C.SuportIcon />
+                Suporte
+              </C.DropdownItem>
+              <C.DropdownItem>
+                <C.ContactIcon />
+                Contato
+              </C.DropdownItem>
+            </C.DropdownContentItems>
+            <C.Line />
+            <C.DropdownContentItems>
+              <C.DropdownItem>
+                <C.NewsIcon />
+                Noticias
+              </C.DropdownItem>
+              <C.DropdownItem>
+                <C.PromotionIcon />
+                Promoção
+              </C.DropdownItem>
+            </C.DropdownContentItems>
+          </C.DrowRowContent>
         </C.DropdownContent>
       </C.DropdownWrapper>
 
