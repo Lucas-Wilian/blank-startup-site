b9c0750121a9cc7907a3cb390e8ed9803a6b2f4c
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-18 17:55:12 -0300

    new component/functions: channelId, ApiKey, Videos Youtube

--- src/components/SectionYoutube/SectionYoutube.tsx
+++ src/components/SectionYoutube/SectionYoutube.tsx
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as C from './styled';
+import ButtonGlogal from '../Button/Button';
+import { getLatestVideos } from '@/utils/getUrlVideo';
+
+interface VideoData {
+  videoTitle: string;
+  videoUrl: string;
+  viewCount: string;
+  description: string;
+}
 
 export default function SectionYoutube() {
+  const channelId = process.env.NEXT_PUBLIC_CHANNEL_ID;
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  const [latestVideos, setLatestVideos] = useState<VideoData[]>([]);
+
+  useEffect(() => {
+    const fetchLatestVideos = async () => {
+      try {
+        if (channelId && apiKey) {
+          const videos = await getLatestVideos(channelId, apiKey, 3);
+          setLatestVideos(videos);
+        } else {
+          console.error('Channel ID is undefined');
+        }
+      } catch (error) {
+        console.error('Erro ao obter vídeos:', error);
+      }
+    };
+
+    fetchLatestVideos();
+  }, [channelId, apiKey]);
+
   return (
     <C.Container>
       <C.ContainerPrimary>
         <C.VideoPrimary>
-          <p>Video Principal</p>
+          {latestVideos.length > 0 && (
+            <>
+              <C.ContainerVideo
+                src={`${latestVideos[0].videoUrl}`}
+                title={latestVideos[0].videoTitle}
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              />
+              <C.ContainerTitleVideo
+                style={{
+                  borderBottom:
+                    ' solid 0.1rem rgba(155, 155, 155, 0.2)',
+                }}
+              >
+                <h3>{latestVideos[0].videoTitle}</h3>
+                <section>
+                  <span>
+                    <C.ViewIcon />
+                    {latestVideos[0].viewCount}
+                  </span>
+                </section>
+              </C.ContainerTitleVideo>
+              <C.ContainerDescriptionVideo>
+                <p>{latestVideos[0].description}</p>
+              </C.ContainerDescriptionVideo>
+            </>
+          )}
         </C.VideoPrimary>
         <C.ContainerSocial>
           <C.SocialSection>
@@ -30,12 +86,44 @@ export default function SectionYoutube() {
         </C.ContainerSocial>
       </C.ContainerPrimary>
       <C.ContainerSecondary>
-        <C.VideoSecondary>
-          <p>Video Secundario</p>
-        </C.VideoSecondary>
-        <C.VideoSecondary>
-          <p>Video Secundario</p>
-        </C.VideoSecondary>
+        {latestVideos.length > 0 && (
+          <C.VideoSecondary>
+            <C.ContainerVideo
+              src={latestVideos[1].videoUrl}
+              title={latestVideos[1].videoTitle}
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            />
+            <C.ContainerTitleVideo>
+              <h3>{latestVideos[1].videoTitle}</h3>
+
+              <section>
+                <span>
+                  <C.ViewIcon />
+                  {latestVideos[1].viewCount}
+                </span>
+              </section>
+            </C.ContainerTitleVideo>
+          </C.VideoSecondary>
+        )}
+        {latestVideos.length > 0 && (
+          <C.VideoSecondary>
+            <C.ContainerVideo
+              src={latestVideos[2].videoUrl}
+              title={latestVideos[2].videoTitle}
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            />
+            <C.ContainerTitleVideo>
+              <h3>{latestVideos[2].videoTitle}</h3>
+
+              <section>
+                <span>
+                  <C.ViewIcon />
+                  {latestVideos[2].viewCount}
+                </span>
+              </section>
+            </C.ContainerTitleVideo>
+          </C.VideoSecondary>
+        )}
       </C.ContainerSecondary>
     </C.Container>
   );
