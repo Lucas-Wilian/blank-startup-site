c29484c7be2bbcb6ac0ec2f9d405e950c5405524
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-18 17:52:37 -0300

    feat: iconViewShow

---
+++ src/components/Icons/IconViewShow.tsx
@@ -0,0 +1,18 @@
+// icon:view-show | Zondicons http://www.zondicons.com | Steve Schoger
+import * as React from 'react';
+
+function IconViewShow(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox='0 0 20 20'
+      fill='currentColor'
+      height='1em'
+      width='1em'
+      {...props}
+    >
+      <path d='M.2 10a11 11 0 0119.6 0A11 11 0 01.2 10zm9.8 4a4 4 0 100-8 4 4 0 000 8zm0-2a2 2 0 110-4 2 2 0 010 4z' />
+    </svg>
+  );
+}
+
+export default IconViewShow;
