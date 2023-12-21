b41b8f646c4e54ff093e95242b5eca17d58ebfc4
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-20 22:56:14 -0300

    init costum sectionPlataform

--- src/components/SectionPlataform/SectionPlataform.tsx
+++ src/components/SectionPlataform/SectionPlataform.tsx
@@ -1,6 +1,43 @@
 import React from 'react';
 import * as C from './styled';
+import Image from 'next/image';
+import ButtonGlogal from '../Button/Button';
 
 export default function SectionPlataform() {
-  return <C.Container>SectionPlataform</C.Container>;
+  return (
+    <C.Container>
+      <C.Main>
+        <C.Title>Multi-Platoforma</C.Title>
+        <C.Content>
+          Atendemos e desenvolvemos para qualquer platoforna do
+          mercado
+        </C.Content>
+        <C.SubTitle>
+          Multi serviços: mobile, web, desktopt, e outros
+        </C.SubTitle>
+        <ButtonGlogal
+          text='Venha conhecer +'
+          font_size='1.7rem'
+          background={false}
+        />
+      </C.Main>
+      <C.ImageContainer>
+        <Image
+          src='https://i.imgur.com/jjd41Ct.png'
+          alt='blank-plataformas-os'
+          width={0}
+          height={0}
+          sizes='100vw'
+          quality={100}
+          // layout='responsive'
+          // objectFit='center'
+          style={{
+            width: '100%',
+            height: '100%',
+            padding: '5px 20px',
+          }}
+        />
+      </C.ImageContainer>
+    </C.Container>
+  );
 }
--- src/components/SectionPlataform/styled.ts
+++ src/components/SectionPlataform/styled.ts
@@ -4,6 +4,30 @@ export const Container = styled.div`
   width: 100%;
   height: 500px;
   padding: 0rem 6rem;
-
-  margin-top: 3rem;
+  margin: 3rem 0;
+  display: flex;
 `;
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 900px;
+  gap: 30px;
+`;
+
+export const Title = styled.h2`
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.colors.blk_theme_btn_bg};
+`;
+
+export const Content = styled.h1`
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.blk_theme_text};
+  font-size: 2.8rem;
+`;
+
+export const SubTitle = styled.span`
+  color: ${({ theme }) => theme.colors.blk_theme_text_code};
+`;
+
+export const ImageContainer = styled.div``;
