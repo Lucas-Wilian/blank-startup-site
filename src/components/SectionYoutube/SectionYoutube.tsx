ea422e0d781947bc185c2a1c6f45f9ed051a587b
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-12-24 21:50:55 -0300

    feat: skeleton loader videos

--- src/components/SectionYoutube/SectionYoutube.tsx
+++ src/components/SectionYoutube/SectionYoutube.tsx
@@ -15,11 +15,12 @@ export default function SectionYoutube() {
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
   const [latestVideos, setLatestVideos] = useState<VideoData[]>([]);
-  const video = false;
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchLatestVideos = async () => {
       try {
+        setLoading(true);
         if (channelId && apiKey) {
           const videos = await getLatestVideos(channelId, apiKey, 3);
           setLatestVideos(videos);
@@ -28,6 +29,8 @@ export default function SectionYoutube() {
         }
       } catch (error) {
         console.error('Erro ao obter vídeos:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -38,79 +41,65 @@ export default function SectionYoutube() {
     <C.Container>
       <C.ContainerPrimary>
         <C.VideoPrimary>
-          {latestVideos.length > 0 && (
+          {loading ? (
             <>
-              {video === false ? (
+              <Skeleton
+                style={{
+                  width: '100%',
+                  height: '280px',
+                }}
+              />
+              <C.ContainerTitleVideo>
+                <h3>
+                  <Skeleton
+                    style={{
+                      height: '35px',
+                    }}
+                  />
+                </h3>
+
                 <Skeleton
-                  baseColor='#202020'
-                  highlightColor='#444'
                   style={{
-                    width: '100%',
-                    height: '280px',
+                    width: '100px',
+                    height: '40px',
                   }}
                 />
-              ) : (
-                <C.ContainerVideo
-                  src={`${latestVideos[0].videoUrl}`}
-                  title={latestVideos[0].videoTitle}
-                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-                />
-              )}
+              </C.ContainerTitleVideo>
+              <C.ContainerDescriptionVideo>
+                <p>
+                  <Skeleton
+                    count={6}
+                    style={{
+                      marginBottom: '5px',
+                      height: '1.1rem',
+                    }}
+                  />
+                </p>
+              </C.ContainerDescriptionVideo>
+            </>
+          ) : (
+            <>
+              <C.ContainerVideo
+                src={latestVideos[0].videoUrl}
+                title={latestVideos[0].videoTitle}
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              />
               <C.ContainerTitleVideo
                 style={{
                   borderBottom:
                     ' solid 0.1rem rgba(155, 155, 155, 0.2)',
                 }}
               >
-                {video === false ? (
-                  <h3>
-                    <Skeleton
-                      baseColor='#202020'
-                      highlightColor='#444'
-                      style={{
-                        //   width: '90px',
-                        height: '35px',
-                      }}
-                    />
-                  </h3>
-                ) : (
-                  <h3>{latestVideos[0].videoTitle}</h3>
-                )}
-
-                {video === false ? (
-                  <Skeleton
-                    baseColor='#202020'
-                    highlightColor='#444'
-                    style={{
-                      width: '100px',
-                      height: '40px',
-                    }}
-                  />
-                ) : (
-                  <section>
-                    <span>
-                      <C.ViewIcon />
-                      {latestVideos[0].viewCount}
-                    </span>
-                  </section>
-                )}
+                <h3>{latestVideos[0].videoTitle}</h3>
+                <section>
+                  <span>
+                    <C.ViewIcon />
+                    {latestVideos[0].viewCount}
+                  </span>
+                </section>
               </C.ContainerTitleVideo>
               <C.ContainerDescriptionVideo>
-                <p>
-                  {video === false ? (
-                    <Skeleton
-                      baseColor='#202020'
-                      highlightColor='#444'
-                      count={6}
-                      style={{
-                        marginBottom: '5px',
-                        height: '1.1rem',
-                      }}
-                    />
-                  ) : (
-                    latestVideos[0].description
-                  )}
-                </p>
+                <p>{latestVideos[0].description}</p>
               </C.ContainerDescriptionVideo>
             </>
           )}
@@ -137,44 +126,98 @@ export default function SectionYoutube() {
         </C.ContainerSocial>
       </C.ContainerPrimary>
       <C.ContainerSecondary>
-        {latestVideos.length > 0 && (
-          <C.VideoSecondary>
-            <C.ContainerVideo
-              src={latestVideos[1].videoUrl}
-              title={latestVideos[1].videoTitle}
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            />
-            <C.ContainerTitleVideo>
-              <h3>{latestVideos[1].videoTitle}</h3>
+        <C.VideoSecondary>
+          {loading ? (
+            <>
+              <Skeleton
+                style={{
+                  width: '100%',
+                  height: '280px',
+                }}
+              />
+              <C.ContainerTitleVideo>
+                <h3>
+                  <Skeleton
+                    style={{
+                      height: '35px',
+                    }}
+                  />
+                </h3>
 
-              <section>
-                <span>
-                  <C.ViewIcon />
-                  {latestVideos[1].viewCount}
-                </span>
-              </section>
-            </C.ContainerTitleVideo>
-          </C.VideoSecondary>
-        )}
-        {latestVideos.length > 0 && (
-          <C.VideoSecondary>
-            <C.ContainerVideo
-              src={latestVideos[2].videoUrl}
-              title={latestVideos[2].videoTitle}
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            />
-            <C.ContainerTitleVideo>
-              <h3>{latestVideos[2].videoTitle}</h3>
+                <Skeleton
+                  style={{
+                    width: '100px',
+                    height: '40px',
+                  }}
+                />
+              </C.ContainerTitleVideo>
+            </>
+          ) : (
+            <>
+              <C.ContainerVideo
+                src={latestVideos[1].videoUrl}
+                title={latestVideos[1].videoTitle}
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              />
+              <C.ContainerTitleVideo>
+                <h3>{latestVideos[1].videoTitle}</h3>
 
-              <section>
-                <span>
-                  <C.ViewIcon />
-                  {latestVideos[2].viewCount}
-                </span>
-              </section>
-            </C.ContainerTitleVideo>
-          </C.VideoSecondary>
-        )}
+                <section>
+                  <span>
+                    <C.ViewIcon />
+                    {latestVideos[1].viewCount}
+                  </span>
+                </section>
+              </C.ContainerTitleVideo>
+            </>
+          )}
+        </C.VideoSecondary>
+        <C.VideoSecondary>
+          {loading ? (
+            <>
+              <Skeleton
+                style={{
+                  width: '100%',
+                  height: '280px',
+                }}
+              />
+              <C.ContainerTitleVideo>
+                <h3>
+                  <Skeleton
+                    style={{
+                      height: '35px',
+                    }}
+                  />
+                </h3>
+
+                <Skeleton
+                  style={{
+                    width: '100px',
+                    height: '40px',
+                  }}
+                />
+              </C.ContainerTitleVideo>
+            </>
+          ) : (
+            <>
+              <C.ContainerVideo
+                src={latestVideos[2].videoUrl}
+                title={latestVideos[2].videoTitle}
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              />
+              <C.ContainerTitleVideo>
+                <h3>{latestVideos[2].videoTitle}</h3>
+
+                <section>
+                  <span>
+                    <C.ViewIcon />
+                    {latestVideos[2].viewCount}
+                  </span>
+                </section>
+              </C.ContainerTitleVideo>
+            </>
+          )}
+        </C.VideoSecondary>
       </C.ContainerSecondary>
     </C.Container>
   );
