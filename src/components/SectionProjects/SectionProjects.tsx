489ffd776c75a674b2f7eeabcfc93008d02cc686
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-01-08 23:15:02 -0300

    new grid

--- src/components/SectionProjects/SectionProjects.tsx
+++ src/components/SectionProjects/SectionProjects.tsx
@@ -1,25 +1,88 @@
 import React from 'react';
 import * as C from './styled';
-import CardProjects from '../CardProjects/CardProjects';
+import Image from 'next/image';
+import img1 from '../../assents/Capturar1.png';
+import img2 from '../../assents/Capturar2.png';
+import img3 from '../../assents/Capturar3.png';
+import img4 from '../../assents/Capturar4.png';
 
 export default function SectionProjects() {
   return (
     <C.Container>
       <C.Title>Nossos principais projetos</C.Title>
       <C.GridProjects>
-        <C.CollumGrid>
-          <C.ContainerImg />
-          <C.ContainerImg />
-        </C.CollumGrid>
-        <C.CollumGridSpan />
-        <C.CollumGrid>
-          <C.ContainerImg />
-          <C.ContainerImg />
-        </C.CollumGrid>
-        <C.CollumGrid>
-          <C.ContainerImg />
-          <C.ContainerImg />
-        </C.CollumGrid>
+        <C.ContainerImg>
+          <Image
+            quality={100}
+            src={img1}
+            objectFit='cover'
+            alt='test'
+            style={{ width: '100%', height: '100%' }}
+          />
+        </C.ContainerImg>
+        <C.ContainerImg>
+          <Image
+            quality={100}
+            src={img2}
+            objectFit='cover'
+            alt='test'
+            style={{ width: '100%', height: '100%' }}
+          />
+        </C.ContainerImg>
+        <C.ContainerImg>
+          <Image
+            quality={100}
+            src={img3}
+            objectFit='cover'
+            alt='test'
+            style={{ width: '100%', height: '100%' }}
+          />
+        </C.ContainerImg>
+        <C.ContainerImg>
+          <Image
+            quality={100}
+            src={img4}
+            objectFit='cover'
+            alt='test'
+            style={{ width: '100%', height: '100%' }}
+          />
+        </C.ContainerImg>
+        <C.ContainerImg>
+          <Image
+            quality={100}
+            src={img1}
+            objectFit='cover'
+            alt='test'
+            style={{ width: '100%', height: '100%' }}
+          />
+        </C.ContainerImg>
+        <C.ContainerImg>
+          <Image
+            quality={100}
+            src={img2}
+            objectFit='cover'
+            alt='test'
+            style={{ width: '100%', height: '100%' }}
+          />
+        </C.ContainerImg>
+        <C.ContainerImg>
+          <Image
+            quality={100}
+            src={img3}
+            objectFit='cover'
+            alt='test'
+            style={{ width: '100%', height: '100%' }}
+          />
+        </C.ContainerImg>
+        <C.ContainerImg>
+          <Image
+            quality={100}
+            src={img4}
+            objectFit='cover'
+            alt='test'
+            style={{ width: '100%', height: '100%' }}
+          />
+        </C.ContainerImg>
       </C.GridProjects>
       <C.BackgroundContainer />
     </C.Container>
